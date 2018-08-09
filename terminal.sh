786791337a8c1c9cf9e7572a2ebb0da79261ef28
worshipentropy <github@worshipentropy.com>
2018-08-09 18:18:13 -0500

    Corrected some xdotool window handling

--- terminal.sh
+++ terminal.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/bash
 
-# Check if gnome-terminal is running.  If not, launch it and store the window ID
+# Check if gnome-terminal is running.  If not, launch it
+
 /usr/bin/pgrep gnome-terminal >/dev/null 2>&1
 if [[ $? -eq 1 ]]
 then
@@ -12,12 +13,16 @@ else
 # Check if terminal is active window
 # If it is not the active window, use xdotool's windowmap function.
 # If it is the active window, use the windowunmap function.
+
 ACTIVEWID=$(xdotool getactivewindow)
 GNOMETERMWID=$(cat /tmp/gnome-term.wid)
 if [[ $ACTIVEWID -ne $GNOMETERMWID ]]
 	then
 		/usr/bin/xdotool windowmap $GNOMETERMWID
+		/usr/bin/xdotool windowmove $GNOMETERMWID 0 0
+		/usr/bin/xdotool windowactivate $GNOMETERMWID
 	else
+#		/usr/bin/xdotool windowminimize $GNOMETERMWID
 		/usr/bin/xdotool windowunmap $GNOMETERMWID
 	fi
 fi
